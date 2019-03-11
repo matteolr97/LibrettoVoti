9de6079d0ca63b274b94406ad0eafd1503a562c3
matte <matte@DESKTOP-JA42G9P>
2019-03-11 13:52:34 +0100

    Metodo Standard

--- src/it/polito/tdp/Voto.java
+++ src/it/polito/tdp/Voto.java
@@ -8,5 +8,32 @@ public class Voto {
 	private int voto;
 	private String nome;
 	private LocalDate data;
+	public Voto(int voto, String nome, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.nome = nome;
+		this.data = data;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
 
+	
+	
+	
 }
